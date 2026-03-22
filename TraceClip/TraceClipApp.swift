baec33e3ed79cad3580b5b0a2dca96a4a74c40d1
Jules Marvin Eguilos <jmeguilos@gmail.com>
2026-03-22 15:10:01 -0700

    Initial Commit

---
+++ TraceClip/TraceClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceClipApp.swift
+//  TraceClip
+//
+//  Created by Marvin on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct TraceClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
